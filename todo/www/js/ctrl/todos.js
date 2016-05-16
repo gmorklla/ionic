543b3d4f65c0325bec3991b494c26a575f68e65d
gmorklla <gmorklla@gmail.com>
2016-05-16 16:03:08 -0500

    Ctrl con comentarios y bosquejo para eliminar taks

--- todo/www/js/ctrl/todos.js
+++ todo/www/js/ctrl/todos.js
@@ -10,8 +10,7 @@
 angular.module('starter')
     .controller('TodoCtrl', ['$scope', '$timeout', '$ionicModal', 'Projects', '$ionicSideMenuDelegate', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
 
-        // A utility function for creating a new project
-        // with the given projectTitle
+        // Fn para crear proyecto
         var createProject = function(projectTitle) {
             var newProject = {
                 title: projectTitle
@@ -24,10 +23,10 @@ angular.module('starter')
         }
 
 
-        // Load or initialize projects
+        // Carga proyectos
         $scope.projects = Projects.all();
 
-        // Called to create a new project
+        // Fn que pide nombre de proyecto y luego llama fn para crear proyecto
         $scope.newProject = function() {
             var projectTitle = prompt('Nombre del Proyecto');
             if (projectTitle) {
@@ -35,19 +34,20 @@ angular.module('starter')
             }
         };
 
-        // Called to select the given project
+        // Fn para seleccionar proyecto
         $scope.selectProject = function(project) {
             $scope.activeProject = project;
             //$ionicSideMenuDelegate.toggleLeft(false);
         };
 
-        // Create our modal
+        // Aquí se crea el modal que se ocupa para crear task
         $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
             $scope.taskModal = modal;
         }, {
             scope: $scope
         });
 
+        // Fn para crear task
         $scope.createTask = function(task) {
             if (!$scope.activeProject || !task) {
                 return;
@@ -76,18 +76,22 @@ angular.module('starter')
             task.title = "";
         };
 
+        // Fn que muestra modal para crear task
         $scope.newTask = function() {
             $scope.taskModal.show();
         };
 
+        // Fn que cierra modal para crear task
         $scope.closeNewTask = function() {
             $scope.taskModal.hide();
         }
 
+        // Fn para abrir menú de Proyectos
         $scope.toggleProjects = function() {
             $ionicSideMenuDelegate.toggleLeft();
         };
 
+        // Fn para remover proyectos
         $scope.removeProject = function () {
             Projects.delete($scope.activeProject).then(function (ref) {
                 if($scope.projects.length > 0) {
@@ -98,12 +102,8 @@ angular.module('starter')
             });
         }
 
-        $scope.onDoubleTap = function () {
-            console.info('Double tap');
-        }
-
+        // Una vez cargados los datos checa si ya existe algún proyecto
         $scope.projects.$loaded().then(function(projects) {
-           console.log(projects.length); // data is loaded here
             if ($scope.projects.length == 0) {
                 while (true) {
                     var projectTitle = prompt('Your first project title:');
@@ -117,6 +117,7 @@ angular.module('starter')
             }
         });
 
+        // Fn para establecer task completada
         $scope.terminada = function (task) {
             if(task.completada) {
                 task.fechaFin = Firebase.ServerValue.TIMESTAMP;
@@ -126,6 +127,17 @@ angular.module('starter')
             var queGuardar = $scope.projects.$getRecord($scope.activeProject.$id);
             $scope.projects.$save(queGuardar).then(function(ref) {
                 //var referencia = ref.key();
+                console.log('Cambio guardado en base');
+            });
+        }
+
+        // Fn para borrar task
+        $scope.borrarTask = function (task) {
+            if(task) {
+                task = null;
+            }
+            var queGuardar = $scope.projects.$getRecord($scope.activeProject.$id);
+            $scope.projects.$save(queGuardar).then(function(ref) {
                 console.log('Cambio guardado en base');
             });
         }
